767efd72254c147f6961643c055b98c428dda504
d5ng <dong5ffice@gmail.com>
2023-07-17 16:16:48 +0900

    CSSTransition

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Modal.css";
-import { Transition } from "react-transition-group";
+import { CSSTransition } from "react-transition-group";
 
 const animationTiming = {
   enter: 400,
@@ -8,20 +8,27 @@ const animationTiming = {
 };
 
 const modal = (props) => {
+  // const cssClass = ["Modal", state === "entering" ? "Modal ModalOpen" : state === "exiting" ? "Modal ModalClosed" : "Modal"];
   return (
-    <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
-      {(state) => {
-        const cssClass = ["Modal", state === "entering" ? "Modal ModalOpen" : state === "exiting" ? "Modal ModalClosed" : "Modal"];
-        return (
-          <div className={cssClass.join(" ")}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
+    <CSSTransition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      classNames={{
+        enter: "",
+        enterActive: "ModalOpen",
+        exit: "",
+        exitActive: "ModalClosed",
       }}
-    </Transition>
+    >
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
